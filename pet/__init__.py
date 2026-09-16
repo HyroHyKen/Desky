8f8b93b3a3ec011c002a0a143d92c19f851eee74
AVTeamway <alexandre.vedrine@teamway.fr>
2026-09-16 16:31:58 +0200

    Release 0.13.0

--- pet/__init__.py
+++ pet/__init__.py
@@ -8,4 +8,4 @@ se fait en une ligne.
 
 APP_NAME = "Desky"
 APP_ID = "Desky.DesktopPet"          # base du mutex nommé (instance unique)
-VERSION = "0.12.0-L14"                # exécutable, icône et installateur
+VERSION = "0.13.0-L15"                # exécutable, icône et installateur
